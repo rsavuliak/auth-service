349bf7e68f33d07d41a569a40eab9b6b302c9218
rsavuliak <savuliak.roman@gmail.com>
2025-06-19 12:43:19 +0100

    Fix API, remove Request Mapping from controller

--- src/main/java/com/example/authservice/controller/AuthController.java
+++ src/main/java/com/example/authservice/controller/AuthController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/")
 public class AuthController {
 
     private final AuthService authService;
