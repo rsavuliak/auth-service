69fa8e9d383e07c6f6293caed50bd437d2040684
rsavuliak <savuliak.roman@gmail.com>
2025-06-20 22:33:33 +0100

    Add Cors

---
+++ src/main/java/com/example/authservice/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.authservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Дозволити для всіх endpoints
+                .allowedOrigins("*") // Дозволити з будь-якого домену
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Які методи дозволені
+                .allowedHeaders("*"); // Які заголовки дозволені
+    }
+}
