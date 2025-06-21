b0d53ac1c48a51d9398e08f23512227239549645
rsavuliak <savuliak.roman@gmail.com>
2025-06-21 17:34:21 +0100

    Add Cors and fix it

--- src/main/java/com/example/authservice/config/SecurityConfig.java
+++ src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.authservice.security.JwtAuthFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,6 +33,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
                         .requestMatchers("/login", "/register", "/refresh").permitAll()
                         .anyRequest().authenticated()
                 )
