c373a2d8c4babbed6ef6f5946916cdb18502c97e
rsavuliak <savuliak.roman@gmail.com>
2025-07-28 18:54:16 +0100

    fix setAllowedOriginPatterns

--- src/main/java/com/example/authservice/config/SecurityConfig.java
+++ src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("https://savuliak.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
