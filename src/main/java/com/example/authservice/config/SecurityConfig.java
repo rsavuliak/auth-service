74cf4e82b292d31e7525dfcfe233791e5573acac
rsavuliak <savuliak.roman@gmail.com>
2025-12-05 23:21:24 +0000

    changed VIRTUAL_HOST

--- src/main/java/com/example/authservice/config/SecurityConfig.java
+++ src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -6,13 +6,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -42,12 +42,12 @@ public class SecurityConfig {
                             if (SecurityContextHolder.getContext().getAuthentication() == null) {
                                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
                             } else {
-                                throw authException; // важливо — дати винятку дійти до контролера
+                                throw authException;
                             }
                         })
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // 🔹 передаємо свою конфігурацію
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
     }
 
